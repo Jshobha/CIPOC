6873a0ba1538c276b358e746262307952fe5bb6e
Quinnoxcsl <harisinghk@quinnox.com>
2015-07-16 11:10:10 +0530

    Name Files
    Name Files

---
+++ Name.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Name :BaseClass
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblMyName.Text = getName();
+        WriteFile();
+    }
+
+    public override string getName()
+    {
+        return "Quinnox Consultancy Service Ltd.";
+    }
+
+    
+}
